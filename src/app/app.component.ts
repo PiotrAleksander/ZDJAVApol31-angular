d0c8edef7b5f4dccce016608225a0a7e302ed2cc
PiotrAleksander <piotrhugonow@gmail.com>
2020-10-05 20:38:16 +0200

    pierwsza lekcja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'punk-app';
+  title = 'punk app';
+  imageURL =
+    'https://pixnio.com/free-images/2018/06/06/2018-06-06-15-55-17-1200x900.jpg';
+  inputModel = 42;
+
+  imageClick(): void {
+    alert('kliknięto obrazek');
+  }
 }
