693a477a46904bd4dcce22fee9d825e73f78b88f
PiotrAleksander <piotrhugonow@gmail.com>
2020-10-06 19:09:41 +0200

    Revert "beer details and list"
    This reverts commit dbf8373c7c814533b940d125258058536dde711e.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Punk App';
+  title = 'punk app';
+  imageURL =
+    'https://pixnio.com/free-images/2018/06/06/2018-06-06-15-55-17-1200x900.jpg';
+  inputModel = 42;
+
+  imageClick(): void {
+    alert('kliknięto obrazek');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,9 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BeerDetailsComponent } from './beer-details/beer-details.component';
-import { BeersListComponent } from './beers-list/beers-list.component';
 
 @NgModule({
-  declarations: [AppComponent, BeerDetailsComponent, BeersListComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
