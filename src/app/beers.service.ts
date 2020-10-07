3686ef1eafa21089f71bbece705eb2d7d8975020
PiotrAleksander <piotrhugonow@gmail.com>
2020-10-07 18:51:34 +0200

    add http call to getBeers

--- src/app/beers.service.ts
+++ src/app/beers.service.ts
@@ -1,18 +1,21 @@
 import { IBeer } from './types/index';
 import { Injectable } from '@angular/core';
-import { BEERS } from './mocks';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+import { BEERS } from './mocks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BeersService {
   baseUrl = 'https://api.punkapi.com/v2/';
+  beersUrl = `${this.baseUrl}beers`;
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
   getBeers(): Observable<IBeer[]> {
-    return of(BEERS);
+    return this.httpClient.get<IBeer[]>(this.beersUrl);
   }
 
   getBeer(id: number): Observable<IBeer> {
